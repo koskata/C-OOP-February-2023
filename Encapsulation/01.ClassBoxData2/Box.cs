da5378700a7fc6f3edb736d83989eebdad52af45
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2023-02-28 16:44:07 +0200

    Adding more exercises for Encapsulation

---
+++ Encapsulation/01.ClassBoxData2/Box.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ClassBoxData2
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double Volume()
+        {
+            return Length * Width * Height;
+        }
+        public double LateralSurfaceArea()
+        {
+            return (2 * Length * Height) + (2 * Width * Height);
+        }
+        public double SurfaceArea()
+        {
+            return (2 * Length * Width) + LateralSurfaceArea();
+        }
+
+
+    }
+}
---
+++ Encapsulation/04.PizzaCalories/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _04.PizzaCalories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dough White Chewy 100
+            try
+            {
+                string[] pizzaTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string[] doughTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Dough dough = new(doughTokens[1], doughTokens[2], double.Parse(doughTokens[3]));
+
+                Pizza pizza = new(pizzaTokens[1], dough);
+                string input;
+                while ((input = Console.ReadLine()) != "END")
+                {
+                    string[] cmdArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    Topping topping = new(cmdArgs[1], double.Parse(cmdArgs[2]));
+
+                    pizza.AddTopping(topping);
+                }
+
+                Console.WriteLine(pizza);
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}

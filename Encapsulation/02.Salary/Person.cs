f83f2bb97c94ed0dea01d4386d7322361fec660f
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2023-02-28 16:43:17 +0200

    Adding exercises for encapsulation

---
+++ Encapsulation/02.Salary/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public void IncreaseSalary(decimal percentage) 
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ Encapsulation/04.Team/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("SoftUni");
+
+            List<Person> persons = new();
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+        }
+    }
+}

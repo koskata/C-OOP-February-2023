45633a2bcabcefd0a2f7cd32177b0710dc951f26
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2023-03-16 18:13:27 +0200

    Adding exercises for polymorphism

---
+++ Polymorphism/03.Shapes/Circle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            private set
+            {
+                radius = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
+    }
+}
